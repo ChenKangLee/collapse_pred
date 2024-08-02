Fixed overfitting by normalizing across all data

run.py
@@ -8,6 +8,7 @@ from utils.util import assure_folder_exist, load_adjacency_matrix, N_GEO_FEATURE
 from model.baseline import FCU
 from model.TGCN import TGCN
 from trainer.supervised import SupervisedTrainer
+from torch.utils.tensorboard import SummaryWriter
 
 
 def train_baseline():
@@ -15,8 +16,10 @@ def train_baseline():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Training on device:", device)
 
+
     # HYPERPARAM
     dataset_name = 'processedFCU_max'
+    experiment_name = 'mix_data_years'
     N_SLOPEUNIT = 38915
     BATCH_SIZE = 12800
     N_EPOCH = 100       # numbers of epoch to train the model
@@ -32,23 +35,30 @@ def train_baseline():
 
     print('Loading baseline model dataset from', dataset_name)
     # for ease of operation, we are using year 102-104 as training, 105 as validation and 106 as test
-    train = DatasetFCU(path_processed, years=range(102, 105), resample='under', normalize=True)
-    valid = DatasetFCU(path_processed, years=range(105, 106), resample='under', normalize=True)
-    # test = DatasetFCU(path_processed, years=range(106, 107), normalize=True)
+    dataset = DatasetFCU(path_processed, years=range(102, 105), resample='under', normalize=True)
+    train, valid, test = random_split(dataset, [0.7, 0.15, 0.15])
 
 
     model = FCU(dim_rain=2, dim_geo=N_GEO_FEATURES, device=device, dropout_rate=0.5)
     loss  = torch.nn.BCEWithLogitsLoss()
-    trainer = SupervisedTrainer(model, loss, tag='FCU', lr=LR, device=device)
+    writer = SummaryWriter(comment=experiment_name)
+    trainer = SupervisedTrainer(model, loss, writer, tag='FCU', lr=LR, device=device)
 
     # train
     print('Begin Training...')
-    train_loss, valid_loss = trainer.train(path_model, train, valid, epochs=N_EPOCH, batch_size=BATCH_SIZE, inspect=f'predictions/{dataset_name}')
+    train_loss, valid_loss = trainer.train(
+        path_model,
+        train,
+        valid,
+        epochs=N_EPOCH,
+        batch_size=BATCH_SIZE,
+        inspect=f'predictions/{dataset_name}_{experiment_name}'
+    )
 
     # check performance of best model
     # path_best = os.path.join(path_model, 'model_epoch_5.pt')
     # print('Testing...')
-    # _, pred = trainer.test(test, batch_size=BATCH_SIZE, model_file=path_best)
+    # _, pred, ground_truth = trainer.test(test, batch_size=BATCH_SIZE, model_file=path_best)
 
 
 def train_pyramid():

trainer/supervised.py
@@ -4,18 +4,17 @@ from tqdm import tqdm
 import numpy as np
 from torch.utils.data import DataLoader, Subset
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
-from torch.utils.tensorboard import SummaryWriter
 from utils.util import assure_folder_exist
 
 
 class SupervisedTrainer:
-    def __init__(self, model, loss, tag='', lr=0.001, device=torch.device('cpu')):
+    def __init__(self, model, loss, writer, tag='', lr=0.001, device=torch.device('cpu')):
         self.device = device
         self.model = model.to(device)
         self.lr = lr
         self.tag = tag
 
-        self.writer = SummaryWriter()
+        self.writer = writer
 
         self.loss = loss
         self.optim = torch.optim.Adam(self.model.parameters(), lr=self.lr)
@@ -79,12 +78,12 @@ class SupervisedTrainer:
             
             if e % 5 == 0:
                 print(f'[Epoch: {e}] Validation:')
-                valid_loss, logits = self.test(valid, batch_size=batch_size, log_metrics=True, epoch=e)
+                valid_loss, logits, ground_truth = self.test(valid, batch_size=batch_size, log_metrics=True, epoch=e)
                 self.writer.add_scalar(self.tag + '/loss/valid', valid_loss, e)
 
                 if inspect:
                     assure_folder_exist(inspect)
-                    tosave = np.concatenate((valid.collapse, logits), axis=1)
+                    tosave = np.concatenate((ground_truth, logits), axis=1)
                     np.savetxt(f'{inspect}/epoch_{e}_valid.txt', tosave, fmt='%.6f')
 
                 # early stopping
@@ -128,34 +127,33 @@ class SupervisedTrainer:
         if model_file:
             self.model.load_state_dict(torch.load(model_file))
 
-        loader = DataLoader(dataset, batch_size=batch_size)
+        loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
         with torch.no_grad():
             accu_loss = 0
             pred = torch.Tensor()
+            label_all = torch.Tensor()
 
             for (_, rain, geo, label) in tqdm(loader):
                 rain = rain.to(self.device)
                 geo = geo.to(self.device)
                 label = label.to(self.device)
 
-                logits = self.model(rain, geo).cpu()
-                pred = torch.cat((pred, probabilities), dim=0)
+                logits = self.model(rain, geo)
+                pred = torch.cat((pred, logits.cpu()), dim=0)
+                label_all = torch.cat((label_all, label.cpu()), dim=0)
 
                 accu_loss += self.loss(logits, label).item()
 
             accu_loss /= len(loader)
 
-            # extract ground truth labels
-            label_true = dataset.collapse
-
             probabilities = torch.sigmoid(pred)
             label_pred = (probabilities > 0.5).float()
 
-            accuracy = accuracy_score(label_true, label_pred)
-            precision = precision_score(label_true, label_pred)
-            recall = recall_score(label_true, label_pred)
-            macro_f1 = f1_score(label_true, label_pred, average='macro')
+            accuracy = accuracy_score(label_all, label_pred)
+            precision = precision_score(label_all, label_pred)
+            recall = recall_score(label_all, label_pred)
+            macro_f1 = f1_score(label_all, label_pred, average='macro')
 
             print(f'loss = {accu_loss:.4f}')
             print(f'Accuracy =', accuracy)
@@ -170,4 +168,4 @@ class SupervisedTrainer:
                 self.writer.add_scalar(self.tag + '/metrics/macro_f1', macro_f1, epoch)
 
 
-        return accu_loss, pred
+        return accu_loss, pred, label_all